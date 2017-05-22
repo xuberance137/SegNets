a7598fbfc7940b39c3d50c298d80cbefcb92168a
Gopal Erinjippurath <gopal.e@gmail.com>
2017-05-22 00:06:31 -0700

    Initial check in of basic segnet model

---
+++ dev/data_loader.py
@@ -0,0 +1,68 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import cv2
+import numpy as np
+import h5py
+import itertools
+
+from helper import *
+import os
+
+# Copy the data to this dir here in the SegNet project /CamVid from here:
+# https://github.com/alexgkendall/SegNet-Tutorial
+DATA_PATH = './CamVid/'
+DATASET_FILE = 'dataset.h5'
+data_shape = 360*480
+
+
+def load_data(mode):
+    data = []
+    label = []
+    with open(DATA_PATH + mode +'.txt') as f:
+        txt = f.readlines()
+        txt = [line.split(' ') for line in txt]
+    for i in range(len(txt)):
+        data.append(np.rollaxis(normalized(cv2.imread(os.getcwd() + txt[i][0][7:])),2))
+        label.append(one_hot_it(cv2.imread(os.getcwd() + txt[i][1][7:][:-1])[:,:,0]))
+        print('.', end='')
+    return np.array(data), np.array(label)
+
+
+train_data, train_label = load_data("train")
+train_label = np.reshape(train_label,(367,data_shape,12))
+
+test_data, test_label = load_data("test")
+test_label = np.reshape(test_label,(233,data_shape,12))
+
+val_data, val_label = load_data("val")
+val_label = np.reshape(val_label,(101,data_shape,12))
+
+
+with h5py.File(DATA_PATH+DATASET_FILE, 'w') as hf:
+    hf.create_dataset("train_data", data=train_data)
+    print('Created train data set')
+    hf.create_dataset("train_label", data=train_label)
+    print('Created train label set')
+    hf.create_dataset("test_data", data=test_data)
+    print('Created test data set')
+    hf.create_dataset("test_label", data=test_label)
+    print('Created test label set')
+    hf.create_dataset("val_data", data=val_data)
+    print('Created validation data set')
+    hf.create_dataset("val_label", data=val_label)
+    print('Created validation data set')
+
+# FYI they are:
+# Sky = [128,128,128]
+# Building = [128,0,0]
+# Pole = [192,192,128]
+# Road_marking = [255,69,0]
+# Road = [128,64,128]
+# Pavement = [60,40,222]
+# Tree = [128,128,0]
+# SignSymbol = [192,128,128]
+# Fence = [64,64,128]
+# Car = [64,0,128]
+# Pedestrian = [64,64,0]
+# Bicyclist = [0,128,192]
+# Unlabelled = [0,0,0]
---
+++ dev/segnet_model_basic.py
@@ -0,0 +1,124 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+from keras.models import Sequential, model_from_json
+from keras.layers.core import Layer, Dense, Dropout, Activation, Flatten, Reshape, Permute
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, UpSampling2D, ZeroPadding2D
+from keras.layers.normalization import BatchNormalization
+from keras.callbacks import ModelCheckpoint, CSVLogger
+from keras import backend as K
+
+import numpy as np
+import json
+import h5py
+import argparse
+from config import *
+
+np.random.seed(007) # starting seed for reproducibility
+
+def parse_and_set_arguments():
+    global TRAINING
+    parser = argparse.ArgumentParser(description='Basic Segnet Model Training and Test App')
+    parser.add_argument('--train', type=int, action='store', dest='TRAINING', default=1, help='Run Segnet in train or test mode(default is test)')
+    args = parser.parse_args()
+    TRAINING = args.TRAINING
+
+def create_model():
+    model = Sequential()
+    # encoding layers
+    model.add(Permute((2,3,1), input_shape=(IMAGE_DATA_SHAPE[2],IMAGE_DATA_SHAPE[0],IMAGE_DATA_SHAPE[1])))
+    model.add(Convolution2D(FILTER_SIZE, kernel_size= KERNEL_SIZE, padding='same', input_shape=IMAGE_DATA_SHAPE))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=POOL_SIZE, padding='valid'))
+
+    model.add(ZeroPadding2D(padding=PAD_SIZE))
+    model.add(Convolution2D(2*FILTER_SIZE, kernel_size= KERNEL_SIZE, padding='valid'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=POOL_SIZE))
+
+    model.add(ZeroPadding2D(padding=PAD_SIZE))
+    model.add(Convolution2D(4*FILTER_SIZE, kernel_size= KERNEL_SIZE, padding='valid'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=POOL_SIZE))
+
+    model.add(ZeroPadding2D(padding=PAD_SIZE))
+    model.add(Convolution2D(8*FILTER_SIZE, kernel_size= KERNEL_SIZE, padding='valid'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+
+    # decoding layers
+    model.add(ZeroPadding2D(padding=PAD_SIZE))
+    model.add(Convolution2D(8*FILTER_SIZE, kernel_size= KERNEL_SIZE, padding='valid'))
+    model.add(BatchNormalization())
+
+    model.add(UpSampling2D(size=POOL_SIZE))
+    model.add(ZeroPadding2D(padding=PAD_SIZE))
+    model.add(Convolution2D(4*FILTER_SIZE, kernel_size= KERNEL_SIZE, padding='valid'))
+    model.add(BatchNormalization())
+
+    model.add(UpSampling2D(size=POOL_SIZE))
+    model.add(ZeroPadding2D(padding=PAD_SIZE))
+    model.add(Convolution2D(2*FILTER_SIZE, kernel_size= KERNEL_SIZE, padding='valid'))
+    model.add(BatchNormalization())
+
+    model.add(UpSampling2D(size=POOL_SIZE))
+    model.add(ZeroPadding2D(padding=PAD_SIZE))
+    model.add(Convolution2D(FILTER_SIZE, kernel_size= KERNEL_SIZE, padding='valid'))
+    model.add(BatchNormalization())
+
+    model.add(Convolution2D(CATEGORIES, kernel_size=(1,1), padding='valid'))
+    # model.add(Reshape((12,360*480), input_shape=(12,360,480)))
+    model.add(Reshape((CATEGORIES, IMAGE_DATA_SHAPE[0]*IMAGE_DATA_SHAPE[1]), input_shape=(CATEGORIES, IMAGE_DATA_SHAPE[0], IMAGE_DATA_SHAPE[1])))
+    model.add(Permute((2,1), input_shape=(12,360*480)))
+    model.add(Activation('softmax'))
+
+    model.summary()
+
+    with open(MODEL_PATH+'basic_model.json', 'w') as outfile:
+        outfile.write(json.dumps(json.loads(model.to_json()), indent=2))
+
+def train_model():
+    # load model architecture
+    with open(MODEL_PATH+'basic_model.json', 'r') as model_file:
+        segnet_basic_model = model_from_json(model_file.read())
+
+    # load training and test data
+    with h5py.File(DATA_PATH+DATASET_FILE, 'r') as hf:
+        train_data = hf['train_data'][:]
+        train_label = hf['train_label'][:]
+        test_data = hf['test_data'][:]
+        test_label = hf['test_label'][:]
+        val_data = hf['val_data'][:]
+        val_label = hf['val_label'][:]
+
+    # defining checkpoints
+    model_checkpoint = ModelCheckpoint(MODEL_PATH+'basic_model_{epoch:03d}.hdf5')
+    csv_log = CSVLogger(MODEL_PATH+'basic_model_training_log.csv', separator=',', append=False)
+    callbacks = [model_checkpoint, csv_log]
+
+    print('Compling Model')
+    segnet_basic_model.compile(optimizer='adadelta', loss='categorical_crossentropy', metrics=['accuracy', 'mae'])
+    print('Training Model')
+    history = segnet_basic_model.fit(train_data, 
+                                    train_label, 
+                                    callbacks=callbacks, 
+                                    batch_size=BATCH_SIZE, 
+                                    epochs=NUM_EPOCH, 
+                                    verbose=1,
+                                    class_weight= CLASS_WEIGHTING,
+                                    validation_data=(val_data, val_label), #val split=1/3
+                                    shuffle=True)
+
+
+if __name__ == '__main__':
+    parse_and_set_arguments()
+    # create_model()
+    train_model()
+
+
+
+
+
