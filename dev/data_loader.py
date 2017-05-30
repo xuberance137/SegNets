20b51af86735f1fc1730283a02251227e4a0e426
Gopal Erinjippurath <gopal.e@gmail.com>
2017-05-29 23:38:34 -0700

    Adding HDF5 data loader script for train/test/val data

--- dev/data_loader.py
+++ dev/data_loader.py
@@ -5,14 +5,34 @@ import numpy as np
 import h5py
 import itertools
 from config import *
-from helper import *
 import os
+from PIL import Image
 
 
 # Copy the data to this dir here in the SegNet project /CamVid from here:
 # https://github.com/alexgkendall/SegNet-Tutorial
 data_shape = 360*480
 
+def normalized(rgb):
+    #return rgb/255.0
+    norm=np.zeros((rgb.shape[0], rgb.shape[1], 3),np.float32)
+
+    b=rgb[:,:,0]
+    g=rgb[:,:,1]
+    r=rgb[:,:,2]
+
+    norm[:,:,0]=cv2.equalizeHist(b)
+    norm[:,:,1]=cv2.equalizeHist(g)
+    norm[:,:,2]=cv2.equalizeHist(r)
+
+    return norm
+
+def one_hot_it(labels):
+    x = np.zeros([360,480,12])
+    for i in range(360):
+        for j in range(480):
+            x[i,j,labels[i][j]]=1
+    return x
 
 def load_data(mode):
     data = []
@@ -21,8 +41,9 @@ def load_data(mode):
         txt = f.readlines()
         txt = [line.split(' ') for line in txt]
     for i in range(len(txt)):
-        data.append(np.rollaxis(normalized(cv2.imread(os.getcwd() + txt[i][0][7:])),2))
-        label.append(one_hot_it(cv2.imread(os.getcwd() + txt[i][1][7:][:-1])[:,:,0]))
+        # data.append(np.rollaxis(normalized(np.asarray(cv2.imread(os.getcwd() + txt[i][0][7:]), dtype="int32")),2))
+        data.append(np.rollaxis(normalized(np.asarray(Image.open(ROOT_FOLDER + 'data/' + txt[i][0][7:]), dtype='uint8')),2))
+        label.append(one_hot_it(cv2.imread(ROOT_FOLDER + 'data/' + txt[i][1][7:][:-1])[:,:,0]))
         print('.', end='')
     return np.array(data), np.array(label)
 
